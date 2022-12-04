fa6b5ce5999af9d43c16d0dfc705f5ec7e1031a5
hamitaksln <hamtaksln@gmail.com>
2022-12-04 15:41:09 +0300

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://hamitaksln.github.io",
+    base: "/",
     // Enable React to support React JSX components.
     integrations: [react(), tailwind({ config: { applyBaseStyles: false } })]
 })
