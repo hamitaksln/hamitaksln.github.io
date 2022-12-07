bda8738ae2bf32a89085a854d3594f14d47d7069
Abdulhamit Akaslan <hamtaksln@gmail.com>
2022-12-07 18:42:49 +0300

    Update redirection on index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config"
 
-// https://astro.build/config
 export default defineConfig({
     site: "https://hamitaksln.github.io",
     base: "/",
-    // Enable React to support React JSX components.
     integrations: [react(), tailwind({ config: { applyBaseStyles: false } })]
 })
