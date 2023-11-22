29515cf1f7e32d610e6ffa4112e67d6698c8e830
Abdulhamit Akaslan <hamtaksln@gmail.com>
2023-11-22 13:57:32 +0300

    Add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,23 @@
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
 import { defineConfig } from "astro/config"
+import partytown from "@astrojs/partytown"
 
+// https://astro.build/config
 export default defineConfig({
     site: "https://hamitaksln.github.io",
     base: "/",
-    integrations: [react(), tailwind({ config: { applyBaseStyles: false } })]
+    integrations: [
+        react(),
+        tailwind({
+            config: {
+                applyBaseStyles: false
+            }
+        }),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"]
+            }
+        })
+    ]
 })
